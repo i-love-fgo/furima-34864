9f9a61afd62e1e8cfb422f4885938e1d6c3cb0cf
i-love-fgo <79355329+i-love-fgo@users.noreply.github.com>
2021-03-15 17:02:06 +0900

    新規登録機能の実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "ユーザー新規登録" do
+    it "nicknameが空だと登録できない" do
+    end
+    it "emailが空だと登録できない" do
+    end
+    it "既に登録されているemailは登録できない" do
+    end
+    it "emailは@を含んでいないと登録できない" do
+    end
+    it "passwordが空だと登録できない" do
+    end
+    it "passwordは6文字以上でないと登録できない" do
+    end
+    it "passwordは半角英数字が混合されていたら、登録できる" do
+    end
+    it "passwordは確認用を含めて、2回入力しないと登録できない" do
+    end
+    it "2つのpasswordが一致しないと、登録できない" do
+    end
+    it "last_nameとfirst_nameは、それぞれ存在しないと登録できない" do
+    end
+    it "last_nameとfirst_nameは、全角での入力でないと登録できない" do
+    end
+    it "last_name_kanaとfirst_name_kanaは、それぞれ存在しないと登録できない" do
+    end
+    it "last_name_kanaとfirst_name_kanaは、全角での入力でないと登録できない" do
+    end
+    it "birthdayが存在しないと登録できない" do
+    end
 end
