c33121f79374bbcd267836ea88eeb7229b2372e0
i-love-fgo <79355329+i-love-fgo@users.noreply.github.com>
2021-03-16 16:15:26 +0900

    単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
-end
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+    validates :nickname, presence: true
+    validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]/}
+    validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]/}
+    validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー-]+\z/}
+    validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー-]+\z/}
+    validates :birthday, presence: true
+  end
