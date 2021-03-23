b669ad6fc88ae141edc85f452b713fcec496919d
i-love-fgo <79355329+i-love-fgo@users.noreply.github.com>
2021-03-23 09:58:45 +0900

    商品購入(Formオブジェクト)実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,14 +1,24 @@
 class BuyersController < ApplicationController
-  before_action :authenticate_user!, except: index
 
   def index
     @buyer = Buyer.new
-  end
-  
-  def new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @buyer_address = BuyerAddress.new(buyer_params)
+    if @buyer_address.valid?
+      @buyer_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def buyer_params
+    params.require(:buyer_address).permit(:postal_code, :area_id, :municipality, :house_number, :building_name, :phone_number, :item_id).merge(token: params[:token], user_id: current_user.id)
   end
 
 end
